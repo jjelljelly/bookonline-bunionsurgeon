d0ca4975231ad5a98c9c7b750a3502ee3a7839ea
Anjelica Wright <55635024+anjelicajwright@users.noreply.github.com>
2023-09-13 13:28:42 +0200

    Update price

--- objects/appointment_types.tsx
+++ objects/appointment_types.tsx
@@ -1,14 +1,14 @@
 const aTypes = {
     'KN-NP': {
         length: 20,
-        price: 200,
+        price: 250,
         consultant: 'Mr. Kaser Nazir',
         type: 'New Consultation',
         description: 'In-Clinic Appointment with Consultant Podiatric Surgeon, Mr. Kaser Nazir.'
     },
     'KN-FU': {
         length: 20,
-        price: 150,
+        price: 200,
         consultant: 'Mr. Kaser Nazir',
         type: 'Follow Up Appointment',
         description: 'Follow Up In-Clinic Appointment with Consultant Podiatric Surgeon, Mr. Kaser Nazir.'
