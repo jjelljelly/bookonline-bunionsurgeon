eb0ae7b7a8094209011c41092825871b83b69a78
jjelljelly <anjelicajwright@hotmail.com>
2025-02-05 15:42:57 +0100

    Update google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import '../styles/globals.css'
+import { setGoogleAnalytics } from '../components/GoogleAnalytics';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    setGoogleAnalytics();
+  }, []);
   return <Component {...pageProps} />
 }
 
